09487e7ebbbed794ace473330883d3278a916608
priyansh18 <singhalpriyansh58@gmail.com>
2021-05-12 19:41:17 +0530

    Send Email in selenium

--- 4.Web Scrapping/Selenium/selenium6.py
+++ 4.Web Scrapping/Selenium/selenium6.py
@@ -2,7 +2,8 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 import pandas as pd
-
+import smtplib
+from email.message import EmailMessage
 
 browser = webdriver.Chrome(ChromeDriverManager().install())
 
@@ -23,13 +24,40 @@ for phoneName in phoneNames:
 for phonePrice in phonePrices:
     myPrice.append(phonePrice.text)
 
-finalList = zip(myPhone,myPrice)
+# Zip two lists and convert data into CSV file
 
-print(finalList)
+finalList = zip(myPhone,myPrice)
 
 df = pd.DataFrame(finalList, columns =['Phone_Name', 'Phone_Price'])
 
-print(df)
-
 df.to_csv('phones.csv')
+
+# Sending Emails 
+
+msg = EmailMessage()
+msg['Subject'] = 'Samsung Scrapped Data'
+msg['From'] = 'Automation Team'
+msg['To'] = 'spriy89@gmail.com'
+
+
+with open('EmailTemplate.txt') as myfile:
+    data = myfile.read()
+    msg.set_content(data)
+
+
+with open("phones.csv","rb") as f:
+    file_data = f.read()
+    print("File data in binary file",file_data)
+    file_name = f.name
+    print('File name is ',file_name)
+    msg.add_attachment(file_data,maintype="application",subtype="csv",filename = file_name)
+
+
+with smtplib.SMTP_SSL('smtp.gmail.com',465) as server:
+    server.login(Email,password)
+    server.send_message(msg)
+
+print('Email Sent')
+
+
 browser.quit()
