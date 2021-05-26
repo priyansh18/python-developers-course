b087a3ff26a8baaead884f76c8a640d3037c5130
priyansh18 <singhalpriyansh58@gmail.com>
2021-05-26 14:06:35 +0530

    Fetched & added Article & Authors models and display it using templates

---
+++ 6.Django/blog/main/views.py
@@ -0,0 +1,41 @@
+from django.http.response import Http404
+from django.shortcuts import get_object_or_404, render
+from . import models
+# Create your views here.
+
+
+def index(request):
+    # Getting first 10 Articles from Articles
+    latest_articles = models.Article.objects.all().order_by('-createdAt')[:10]
+    # This query won't hit the database unless this variable where it is stored in used somewhere so we can say it as a lazy query
+    # print(latest_articles)
+    context = {
+        'latest_articles': latest_articles
+    }
+    return render(request, 'main/index.html', context)
+
+
+def article(request, pk):
+    # try:
+    #     article = models.Article.objects.get(pk=pk)
+    # except:
+    #     raise Http404
+
+    article = get_object_or_404(models.Article.objects, pk=pk)
+
+    context = {
+        'article': article
+    }
+
+    return render(request, 'main/article.html', context)
+
+
+def author(request, pk):
+
+    author = get_object_or_404(models.Author.objects, pk=pk)
+
+    context = {
+        'author': author,
+    }
+
+    return render(request, 'main/author.html', context)
