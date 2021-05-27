247421f5a7c5da03ac03b22ed2dcb0f763f7ef99
priyansh18 <singhalpriyansh58@gmail.com>
2021-05-27 21:08:46 +0530

    Generic views to create,update,detail content in databas using class based views

--- 6.Django/generic_views/main/urls.py
+++ 6.Django/generic_views/main/urls.py
@@ -3,5 +3,11 @@ from . import views
 
 # as_view convert class based views into function based view
 urlpatterns = [
-    path('',views.Index.as_view(),name='index')
+    path('',views.Index.as_view(),name='index'),
+    path('college/<int:pk>',views.CollegeDetail.as_view(),name='college_detail'),
+    path('college',views.CollegeList.as_view(),name='college_list'),
+    path('create_college',views.CollegeCreate.as_view(),name='create_college'),
+    path('update_college/<int:pk>',views.CollegeUpdate.as_view(),name='update_college'),
+    path('delete_college/<int:pk>',views.CollegeDelete.as_view(),name='delete_college'),
+    path('create_student',views.StudentCreate.as_view(),name='create_student'),
 ]
--- 6.Django/generic_views/main/views.py
+++ 6.Django/generic_views/main/views.py
@@ -2,6 +2,8 @@ from django.http.request import HttpRequest
 from django.http.response import HttpResponse
 from django.shortcuts import render
 from django.views import View
+from django.views.generic import (DetailView,ListView,CreateView,UpdateView,DeleteView)
+from .models import Student,College
 
 # Create your views here.
 class Index(View):
@@ -10,3 +12,47 @@ class Index(View):
     
     def post(request,self):
         return HttpResponse("POST Request") 
+
+# Detail View
+class CollegeDetail(DetailView):
+    model = College
+    template_name = 'main/college_detail.html'
+
+# List View
+class CollegeList(ListView):
+    model = College
+    template_name = 'main/college_list.html'
+    # Instead of Object we can change its name to anything we want
+    context_object_name = 'colleges'
+
+# Create View
+
+class CollegeCreate(CreateView):
+    model = College
+    template_name = 'main/college_create.html'
+    fields = "__all__"
+    success_url = '/college'
+
+class StudentCreate(CreateView):
+    model = Student
+    template_name = 'main/student_create.html'
+    fields = "__all__"
+    success_url = '/college'
+
+# Update View
+
+class CollegeUpdate(UpdateView):
+    model = College
+    template_name = 'main/college_create.html'
+    fields = "__all__"
+    success_url = '/college'
+
+class CollegeDelete(DeleteView):
+    model = College
+    template_name = 'main/confirm.html'
+    fields = "__all__"
+    success_url = '/college'
+
+
+
+# Delete View
