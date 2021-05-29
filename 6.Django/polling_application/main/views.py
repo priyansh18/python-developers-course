481592d8c6a516565b9aa8942ff3a14a09a93cb5
priyansh18 <singhalpriyansh58@gmail.com>
2021-05-29 21:51:27 +0530

    Created Questions & Choices models and display questions in homepage

--- 6.Django/polling_application/main/views.py
+++ 6.Django/polling_application/main/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView
+from .models import Question,Choice
 # Create your views here.
+
+
+class Index(ListView):
+    model = Question
+    template_name = 'main/index.html'
+    # Default context object name is smallm model name _ list i.e. question_list 
+    
--- 6.Django/polling_application/poler/settings.py
+++ 6.Django/polling_application/poler/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main'
     # Auth won't be using Database so not used in Installed Appps
 ]
 
