35a07240408c771d6572ddbaf44e9c4760eec388
priyansh18 <singhalpriyansh58@gmail.com>
2021-05-30 19:22:00 +0530

    Convert data into JSON by using django serializers

--- 6.Django/REST_API/api/views.py
+++ 6.Django/REST_API/api/views.py
@@ -1,23 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from .serializers import StudentSerializer 
 
 # Create your views here.
 # Using of api_view decorators convert list or object into type-JSON
+class Student:
+    def __init__(self,name,roll_no,marks):
+        self.name = name
+        self.roll_no = roll_no
+        self.marks = marks
+
 @api_view()
 def usersApi(request):
     # Can also be get through database
-    users = [
-        {
-            'name':'Priyansh Singhal',
-            'language' : 'Python'
-        },
-        {
-            'name':'Shaurya Singhal',
-            'language' : 'C++'
-        }
-    ]
+    student1 = Student("Priyansh",1,100)
+    student2 = Student("Shaurya",2,90)
+    student3 = Student("Aman",3,96)
+    response = StudentSerializer([student1,student2,student3],many = True)
 
-    return Response(users)
+    return Response(response.data)
