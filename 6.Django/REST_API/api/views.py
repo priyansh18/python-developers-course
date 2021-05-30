d213ac98125d42de94db22204f88b161491ccdb9
priyansh18 <singhalpriyansh58@gmail.com>
2021-05-30 19:03:45 +0530

    convert list of object to JSON format using Response and decorator

--- 6.Django/REST_API/api/views.py
+++ 6.Django/REST_API/api/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 # Create your views here.
+# Using of api_view decorators convert list or object into type-JSON
+@api_view()
+def usersApi(request):
+    # Can also be get through database
+    users = [
+        {
+            'name':'Priyansh Singhal',
+            'language' : 'Python'
+        },
+        {
+            'name':'Shaurya Singhal',
+            'language' : 'C++'
+        }
+    ]
+
+    return Response(users)
