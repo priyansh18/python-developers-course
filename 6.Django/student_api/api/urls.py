217432324bce2a7fafb1c5628d066eb477aa7e22
priyansh18 <singhalpriyansh58@gmail.com>
2021-06-01 20:47:32 +0530

    Generic views for CRUD operation on student model

--- 6.Django/student_api/api/urls.py
+++ 6.Django/student_api/api/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('',views.api,name='api')
+    path('students/',views.StudentListView.as_view(),name='students_list'),
+    path('student/<int:pk>',views.StudentDetailView.as_view(),name='student'),
+    path('deletestudent/<int:pk>',views.StudentDeleteView.as_view(),name='student_delete'),
 ]
--- 6.Django/student_api/api/views.py
+++ 6.Django/student_api/api/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
-
+from rest_framework.response import Response
+from . import models,serializers
+from rest_framework.generics import (ListAPIView, RetrieveAPIView, UpdateAPIView,
+                                     RetrieveUpdateAPIView, CreateAPIView, ListCreateAPIView, DestroyAPIView)
 # Create your views here.
+
+# class StudentListView(ListAPIView):
+#     queryset = models.Student.objects.all()
+#     serializer_class = serializers.StudentSerializer
+
+# Instead of using ListAPIView we use ListCreateAPIView If Get request is sent it shows data and if post request is sent then create data
+class StudentListView(ListCreateAPIView):
+    queryset = models.Student.objects.all()
+    serializer_class = serializers.StudentSerializer
+
+# Instead of using RetrieveAPIView we use RetrieveUpdateAPIView  If Get request is sent it shows data and if patch request is sent with updated data it changes the data of particular id 
+class StudentDetailView(RetrieveUpdateAPIView):
+    queryset = models.Student.objects.all()
+    serializer_class = serializers.StudentSerializer
+
+class StudentDeleteView(DestroyAPIView):
+    queryset = models.Student.objects.all()
+    serializer_class = serializers.StudentSerializer
